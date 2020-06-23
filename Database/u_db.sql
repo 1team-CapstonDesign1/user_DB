11f299f168b36ef20831ef47e6e80fa0b179d281
jisup <xrl0603@naver.com>
2020-06-23 19:51:43 +0900

    Create u_db.sql

---
+++ Database/u_db.sql
@@ -0,0 +1,12 @@
+CREATE TABLE u_db (
+	u_num INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	u_name VARCHAR(80) NOT NULL,
+	u_age INT NOT NULL,
+	u_gender INT NOT NULL,
+	u_job VARCHAR(80) NOT NULL,
+    u_bathing INT NOT NULL,
+    u_water INT NOT NULL,
+    u_temperature INT NOT NULL,
+    u_time INT NOT NULL,
+    u_last TINYINT NOT NULL
+);
