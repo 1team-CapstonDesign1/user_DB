9af31613c53d55c2aed9383e670d5531bf31f12a
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-06-23 20:56:55 +0900

    Update u_db.sql

--- Database/u_db.sql
+++ Database/u_db.sql
@@ -4,9 +4,9 @@ CREATE TABLE u_db (
 	u_age INT NOT NULL,
 	u_gender INT NOT NULL,
 	u_job VARCHAR(80) NOT NULL,
-    u_bathing INT NOT NULL,
-    u_water INT NOT NULL,
-    u_temperature INT NOT NULL,
-    u_time INT NOT NULL,
-    u_last TINYINT NOT NULL
+    	u_bathing INT NOT NULL DEFAULT '0',
+    	u_water INT NOT NULL DEFAULT '0',
+    	u_temperature INT NOT NULL DEFAULT '0',
+    	u_time INT NOT NULL DEFAULT '0',
+    	u_last TINYINT NOT NULL DEFAULT '0'
 );
