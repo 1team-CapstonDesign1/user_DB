241bbbf8b6dc9b99e20aaaf7745ca7002d903766
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-06-23 21:01:57 +0900

    Update online_db.sql

--- Database/online_db.sql
+++ Database/online_db.sql
@@ -3,11 +3,11 @@ Create Table online_db(
 	s_date DATE NOT NULL,
 	s_age INT NOT NULL,
 	s_gender INT NOT NULL,
-    s_temperature INT NOT NULL,
-    s_start INT NOT NULL,
-    s_during INT NOT NULL,
-    s_end INT NOT NULL,
-    s_perfume CHAR(32) NOT NULL,
+    	s_temperature INT NOT NULL,
+    	s_start INT NOT NULL,
+    	s_during INT NOT NULL,
+    	s_end INT NOT NULL,
+    	s_perfume CHAR(32) NOT NULL,
 	s_job VARCHAR(80) NOT NULL,
-    s_weather INT NOT NULL
+    	s_weather INT NOT NULL
 );
